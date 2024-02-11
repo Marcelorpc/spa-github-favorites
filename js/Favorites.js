2e49bb0b8876d1d797cc084e12e96189fee3445c
marcelorpc <marcelorpchaves@gmail.com>
2024-02-11 19:39:03 -0300

    add class favorite and favoriteviews

---
+++ js/Favorites.js
@@ -0,0 +1,12 @@
+class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+  }
+}
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+    alert('oi')
+  }
+}
